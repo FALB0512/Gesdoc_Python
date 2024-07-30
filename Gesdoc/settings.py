0f1cbd09858d7cd54fd8b11525bbd58c9060b65f
FARUD <farudlopez36@gmail.com>
2024-07-29 20:17:56 -0500

    Reconfiguración de buildpacks

--- Gesdoc/settings.py
+++ Gesdoc/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Gesdoc_Sena',
+    'Modulos.Gesdoc_Sena',
 ]
 
 MIDDLEWARE = [
